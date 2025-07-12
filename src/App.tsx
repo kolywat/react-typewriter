9ec5f005131c2be9ea254cabad22bb7c95fd277f
kolywat <maxnem93@gmail.com>
2025-07-12 03:02:47 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import Typewriter from "./src/Typewriter/Typewriter";
 
 const App: React.FC = () => {
   return (
-    <div>
+    <div className="typewriter-container">
       <Typewriter
         text={["Hello, World!", "Welcome to the Typewriter Effect!"]}
-        speed={100}
+        speed={50}
         loop={false}
         random={50}
         delay={500}
